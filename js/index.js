0bee0ad5823766c5d2072a18b394b0a5f0cd3403
AivarasBondarenko <aivarasbondarenko@gmail.com>
2022-03-14 20:17:49 +0200

    Completed tips calculator task

---
+++ js/index.js
@@ -0,0 +1,22 @@
+console.log("hello");
+
+let userInput = parseInt(prompt("Please enter a random number"));
+let tips, text, amount; 
+
+if( userInput < 20){
+    tips = userInput * 0.1;
+} else if( userInput >= 20 && userInput < 50) {
+    tips = userInput * 0.075;
+}else if(userInput < 0){
+    console("Amount can't be negative");
+    text = 0;
+} else { 
+    tips = userInput * 0.05;
+}
+
+amount = userInput + tips;
+
+if( text != 0 ){
+    text = `Amount: ${amount} tips: ${tips} initial amount: ${userInput}`; 
+    console.log(text);
+}
