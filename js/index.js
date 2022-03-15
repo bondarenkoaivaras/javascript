3cf7e96523a230f04d2aa11e6770c9f5aa05e566
AivarasBondarenko <aivarasbondarenko@gmail.com>
2022-03-15 18:39:24 +0200

    array methods

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
-console.log("hello");
+// console.log("hello");
 
-let userInput = parseInt(prompt("Please enter a random number"));
-let tips, text, amount; 
+// let userInput = parseInt(prompt("Please enter a random number"));
+// let tips, text, amount; 
 
-if( userInput < 20){
-    tips = userInput * 0.1;
-} else if( userInput >= 20 && userInput < 50) {
-    tips = userInput * 0.075;
-}else if(userInput < 0){
-    console("Amount can't be negative");
-    text = 0;
-} else { 
-    tips = userInput * 0.05;
-}
+// if( userInput < 20){
+//     tips = userInput * 0.1;
+// } else if( userInput >= 20 && userInput < 50) {
+//     tips = userInput * 0.075;
+// }else if(userInput < 0){
+//     console("Amount can't be negative");
+//     text = 0;
+// } else { 
+//     tips = userInput * 0.05;
+// }
 
-amount = userInput + tips;
+// amount = userInput + tips;
 
-if( text != 0 ){
-    text = `Amount: ${amount} tips: ${tips} initial amount: ${userInput}`; 
-    console.log(text);
-}
+// if( text != 0 ){
+//     text = `Amount: ${amount} tips: ${tips} initial amount: ${userInput}`; 
+//     console.log(text);
+// }
+
+let baldai = ["lempa", "lentynos", "sofa", "stalas"];
+
+baldai.pop();
+baldai.shift();
+baldai.push("kede");
+baldai.unshift('spintele');
+
+baldai.slice(2, 1, "taure", "sakute");
+
+console.log(baldai);
